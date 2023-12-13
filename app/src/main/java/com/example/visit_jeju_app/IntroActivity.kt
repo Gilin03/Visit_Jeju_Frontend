0f61de783dc86c4130941fe7c76a8a11c6c878b8
gilin03 <tb081212@naver.com>
2023-12-13 16:30:08 +0900

    휴대폰 인증 추가 (회원가입화면 정리필요)

--- app/src/main/java/com/example/visit_jeju_app/IntroActivity.kt
+++ app/src/main/java/com/example/visit_jeju_app/IntroActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.visit_jeju_app.login.AuthActivity
+import com.example.visit_jeju_app.login.PhoneAuthActivity
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,7 @@ class IntroActivity : AppCompatActivity() {
 
         // Handler를 사용하여 3초 후에 AuthActivity로 이동
         Handler().postDelayed({
-            val intent = Intent(this, AuthActivity::class.java)
+            val intent = Intent(this, PhoneAuthActivity::class.java)
             startActivity(intent)
             finish() // IntroActivity를 종료하여 뒤로 가기 버튼을 눌러 이전 화면으로 돌아갈 수 없도록 함
         }, 3000) // 3초(3000 밀리초) 후에 실행
